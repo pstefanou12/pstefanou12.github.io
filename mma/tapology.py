5012ade8bd8ed63d5549cb5779d1b3f89395cc33
Patroklos <patstefanou@gmail.com>
2025-10-13 11:54:56 -0700

    Add a table of contents section to the recaps. Update tapology script to create links to each section + table of contents. Update mma.css to handle the table of contents section.

--- mma/tapology.py
+++ mma/tapology.py
@@ -54,9 +54,11 @@ def scrape_tapology_event(url):
                 card_placement = bout_info[-1].find_all('a')[0].get_text().strip()
 
                 if 'main' in card_placement.lower(): 
-                    card_placement = 'Main'
+                    card_placement = 'Main Card'
                 elif card_placement.lower() == 'prelim': 
-                    card_placement = 'Prelim'
+                    card_placement = 'Prelims'
+                elif 'early' in card_placement.lower():
+                    card_placement = 'Early Prelims'
 
                 bout_dict = {
                     'card_placement': card_placement,
@@ -119,19 +121,44 @@ def generate_html_template(event_data, rating='0.0', rating_class='poor'):
     
     <div class="recap-content">
 
-      <h2>Summary</h2>
+      <h2 id="table-of-contents">Table of Contents</h2>
+      <ul class="toc">
+'''
+    
+    # Add table of contents links for Summary and all fights
+    html += '        <li><a href="#summary">Summary</a></li>\n'
+    
+    for section, fights in sections.items():
+        section_id = section.lower().replace(' ', '-')
+        html += f'        <li><strong>{section}</strong>\n'
+        html += '          <ul>\n'
+        
+        for fight in fights:
+            # Create a unique ID for each fight
+            fight_id = f"{fight['fighter1'].lower().replace(' ', '-')}-vs-{fight['fighter2'].lower().replace(' ', '-')}"
+            html += f'            <li><a href="#{fight_id}">{fight["fighter1"]} vs. {fight["fighter2"]}</a></li>\n'
+        
+        html += '          </ul>\n'
+        html += '        </li>\n'
+    
+    html += '      </ul>\n      <br>\n\n'
+    
+    html += '''      <h2 id="summary">Summary</h2>
         [Add your event summary here]
         <br>
         <br>
 
 '''
     
-    # Add fights by section
+    # Add fights by section with anchor IDs
     for section, fights in sections.items():
-        html += f'      <h2>{section}</h2>\n      \n'
+        section_id = section.lower().replace(' ', '-')
+        html += f'      <h2 id="{section_id}">{section}</h2>\n      \n'
         
         for fight in fights:
-            html += f'''      <div class="fight">
+            # Create the same unique ID for each fight
+            fight_id = f"{fight['fighter1'].lower().replace(' ', '-')}-vs-{fight['fighter2'].lower().replace(' ', '-')}"
+            html += f'''      <div class="fight" id="{fight_id}">
         <h3>{fight['fighter1']} vs. {fight['fighter2']}</h3>
         <p class="fight-result"><strong>Result: </strong>{fight['method_of_victory']} at {fight['time_of_victory']}</p>
         <p>
@@ -143,37 +170,38 @@ def generate_html_template(event_data, rating='0.0', rating_class='poor'):
     
     html += '''    </div>
   </div>
-      <div class="footer">
-      <div class="footer-side" id="left">
-        <div>Copyright © Patroklos Stefanou</div>
-      </div>
-      <div class="social-media">
-        <a class="platform" target="_blank" href="https://www.linkedin.com/in/patroklos-stefanou/", rel="noopener noreferrer">
-          <svg xmlns="http://www.w3.org/2000/svg", viewBox="0 0 24 24">
-            <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"/>
-          </svg>
-        </a>
-        <a class="platform" target="_blank" href="https://github.com/pstefanou12">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"/>
-          </svg>
-        </a>
-        <a class="platform" target="_blank" href="https://x.com/formyxscarfalo1/">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"/>
-              <circle cx="4" cy="4" r="2"/>
-          </svg>        
-        </a>
-        <a class="platform" target="_blank" href="https://www.strava.com/athletes/23695440">
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M15.387 17.944l-2.089-4.116h-3.065L15.387 24l5.15-10.172h-3.066m-7.008-5.599l2.836 5.598h4.172L10.463 0l-7 13.828h4.169"/>
-          </svg>
-        </a>
-      </div>
-      <div class="footer-side" id="right">
-        <div>Email: <a href="mailto:patstefanou@gmail.com">patstefanou@gmail.com</a></div>
-      </div>
+  
+  <div class="footer">
+    <div class="footer-side" id="left">
+      <div>Copyright © Patroklos Stefanou</div>
     </div>
+    <div class="social-media">
+      <a class="platform" target="_blank" href="https://www.linkedin.com/in/patroklos-stefanou/" rel="noopener noreferrer">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"/>
+        </svg>
+      </a>
+      <a class="platform" target="_blank" href="https://github.com/pstefanou12">
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"/>
+        </svg>
+      </a>
+      <a class="platform" target="_blank" href="https://x.com/formyxscarfalo1/">
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"/>
+          <circle cx="4" cy="4" r="2"/>
+        </svg>        
+      </a>
+      <a class="platform" target="_blank" href="https://www.strava.com/athletes/23695440">
+        <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.387 17.944l-2.089-4.116h-3.065L15.387 24l5.15-10.172h-3.066m-7.008-5.599l2.836 5.598h4.172L10.463 0l-7 13.828h4.169"/>
+        </svg>
+      </a>
+    </div>
+    <div class="footer-side" id="right">
+      <div>Email: <a href="mailto:patstefanou@gmail.com">patstefanou@gmail.com</a></div>
+    </div>
+  </div>
   
 </body>
 </html>'''
@@ -212,7 +240,7 @@ def main():
     html_content = generate_html_template(event_data, rating, rating_class)
     
     # Save to file
-    filename = './mma/recaps/' f"{event_data['event_name'].replace(' ', '_')}_recap.html"
+    filename = f"./mma/recaps/{event_data['event_name'].replace(' ', '_')}_recap.html"
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(html_content)
     
